2865e66547a40d49544b6dcb6df5ce940b78daad
Romina <romina.barraza@infinixsoft.com>
2019-05-16 19:07:49 -0300

    Color testing

--- app/src/main/java/com/visight/adondevamos/ui/start/SplashActivity.kt
+++ app/src/main/java/com/visight/adondevamos/ui/start/SplashActivity.kt
@@ -1,4 +1,25 @@
 package com.visight.adondevamos.ui.start
 
-class SplashActivity {
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.visight.adondevamos.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //transparent status bar on android Kitkat onwards
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window // in Activity's onCreate() for instance
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+    }
 }
