83f50451e4d8b3db283a2bb1bbd1961ab7d5a19b
Romina <romina.barraza@infinixsoft.com>
2019-11-18 04:59:19 -0300

    Edits

--- app/src/main/java/com/visight/adondevamos/ui/main/place/PlaceDetailActivity.kt
+++ app/src/main/java/com/visight/adondevamos/ui/main/place/PlaceDetailActivity.kt
@@ -22,6 +22,18 @@ import kotlinx.android.synthetic.main.activity_place_detail.*
 import kotlinx.android.synthetic.main.layout_place_detail_content.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 import java.math.RoundingMode
+import android.R.attr.data
+import com.github.mikephil.charting.charts.LineChart
+import android.graphics.Color
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.data.LineDataSet
+
 
 class PlaceDetailActivity : BaseActivity(), PlaceDetailActivityContract.View{
     var mPresenter: PlaceDetailActivityContract.Presenter? = null
@@ -52,6 +64,46 @@ class PlaceDetailActivity : BaseActivity(), PlaceDetailActivityContract.View{
                 .load(mPublicPlace!!.icon)
                 .into(ivHeader)
         }
+
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f)
+        chart.setBackgroundColor(Color.rgb(104, 241, 175))
+
+        // no description text
+        chart.getDescription().setEnabled(false)
+
+        // enable touch gestures
+        chart.setTouchEnabled(true)
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true)
+        chart.setScaleEnabled(true)
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false)
+
+        chart.setDrawGridBackground(false)
+        chart.setMaxHighlightDistance(300f)
+
+        var x = chart.getXAxis()
+        x.setEnabled(false)
+
+        var y = chart.getAxisLeft()
+        y.setLabelCount(6, false)
+        y.setTextColor(Color.WHITE)
+        y.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+        y.setDrawGridLines(false)
+        y.setAxisLineColor(Color.WHITE)
+
+        chart.getAxisRight().setEnabled(false)
+
+        chart.getLegend().setEnabled(false)
+
+        chart.animateXY(2000, 2000)
+
+        // don't forget to refresh the drawing
+        chart.invalidate()
+
+        setData(100, 200f)
 
         btnGoToReport.setOnClickListener {
             val intent = Intent(this@PlaceDetailActivity, ReportFromPlaceActivity::class.java)
@@ -115,4 +167,49 @@ class PlaceDetailActivity : BaseActivity(), PlaceDetailActivityContract.View{
     override fun onDestroy() {
         super.onDestroy()
     }
+
+    private fun setData(count: Int, range: Float) {
+
+        val values : MutableList<Entry> = mutableListOf()
+
+        for (i in 0 until count) {
+            val `val` = (Math.random() * (range + 1)).toFloat() + 20
+            values.add(Entry(i.toFloat(), `val`))
+        }
+
+        val set1: LineDataSet
+
+        if (chart.data != null && chart.data.dataSetCount > 0) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "DataSet 1")
+
+            set1.mode = LineDataSet.Mode.CUBIC_BEZIER
+            set1.cubicIntensity = 0.2f
+            set1.setDrawFilled(true)
+            set1.setDrawCircles(false)
+            set1.lineWidth = 1.8f
+            set1.circleRadius = 4f
+            set1.setCircleColor(Color.WHITE)
+            set1.highLightColor = Color.rgb(244, 117, 117)
+            set1.color = Color.WHITE
+            set1.fillColor = Color.WHITE
+            set1.fillAlpha = 100
+            set1.setDrawHorizontalHighlightIndicator(false)
+            set1.fillFormatter = IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+
+            // create a data object with the data sets
+            val data = LineData(set1)
+            data.setValueTextSize(9f)
+            data.setDrawValues(false)
+
+            // set data
+            chart.data = data
+        }
+    }
+
 }
